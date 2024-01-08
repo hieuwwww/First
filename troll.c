1318cac45bc0bc65de66b154a5713258edfd024b
hieuwwww <128878811+hieuwwww@users.noreply.github.com>
2024-01-08 18:19:17 +0700

    Add files via upload

---
+++ troll.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+
+
+int main(){
+	int n;
+	printf("nhap n: ");
+	scanf("%d",&n);
+	int m;
+	printf("nhap m: ");
+	scanf("%d",&m);
+	int cnt=1;
+	float l=m;
+	while (m>=10){
+		cnt++;
+		m=m/10;
+	}
+	l=l/pow(10,cnt);
+	float flt;
+	if (n<0){
+		flt = n-l;
+	}else{flt=n+l;
+	}
+	
+	printf("\n%f",flt);
+	
+
+}
